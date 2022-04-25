8a6b81b7309567f78e668e075ce0589050ceb7e5
doroteea <doroteeasandor@yahoo.com>
2022-04-26 00:27:48 +0300

    tutorial done

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,4 +1,4 @@
-#include <jni.h>
+﻿#include <jni.h>
 #include <string>
 
 extern "C" JNIEXPORT jstring JNICALL
@@ -8,27 +8,82 @@ Java_com_example_phoroeditorapplication2_MainActivity_stringFromJNI(
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
+
+int truncate(int val){
+    if(val < 0){
+        return 0;
+    }
+    if(val > 255){
+        return 255;
+    }
+    return val;
+}
+int truncateBW(int val){
+    if(val < 128){
+        return 0;
+    }
+    return 255;
+}
+
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_example_phoroeditorapplication2_MainActivity_blackAndWhite(JNIEnv *env, jclass clazz,
                                                                     jintArray pixels, jint width,
                                                                     jint height) {
     jint *pixels_ = (*env).GetIntArrayElements(pixels,NULL);
-//    jsize len = (*env).GetArrayLength(pixels);
-//    for(int i=0;i<len;i++){
-//        pixels_[i]/=2;
-//    }
-   unsigned char *colors = (unsigned char *) pixels_;
-    int pixelCount = width * height *4; //char
-    int i=0;
-    while(i<pixelCount){
-        unsigned char average = (colors[i]+ colors[i+1] + colors[i+2])/3; //RGB
-        colors[i] = average;
-        colors[i+1] = average;
-        colors[i+2] = average;
-        i+=4;
+    unsigned char *colors = (unsigned char *) pixels_;
+    for(int y = 0;y<height;y++){
+        for(int x = 0;x<width;x++){
+            for(int c = 0; c< 4;c++){
+                int yDif = y - height/2;
+                int xDif = x - width/2;
+                int rotation = 100*(3.14/180);
+
+                double radian = sqrt(pow(xDif,2)+pow(yDif,2));
+                double cal = atan2(yDif,xDif) + rotation*(((height/2)-radian)/(height/2));
+
+                int rotationY = (height/2) + (int)(sin(cal)*radian);
+                int rotationX = (width/2) + (int)(cos(cal)*radian);
+
+                if(rotationY > height-1){
+                    rotationY = height-1;
+                } else if(rotationY < 0){
+                    rotationY = 0;
+                }
+                if(rotationX > width-1){
+                    rotationX = width-1;
+                }else
+                if(rotationX < 0){
+                    rotationX = 0;
+                }
+                //colors[(y*width+x)*4+c] = colors[(rotationY*width+rotationX)*4+c];
+                pixels_[rotationY*width*4+rotationX*4+c] = pixels_[y*width*4+x*4+c];
+            }
+        }
     }
     (*env).ReleaseIntArrayElements(pixels,pixels_,0);
-//printf("Je suis");
 
 }
+
+//Java_com_example_phoroeditorapplication2_MainActivity_blackAndWhite(JNIEnv *env, jclass clazz,
+//        jintArray pixels, jint width,
+//        jint height) {
+//jint *pixels_ = (*env).GetIntArrayElements(pixels,NULL);
+////    jsize len = (*env).GetArrayLength(pixels);
+////    for(int i=0;i<len;i++){
+////        pixels_[i]/=2;
+////    }
+//unsigned char *colors = (unsigned char *) pixels_;
+//int pixelCount = width * height *4; //char
+//int i=0;
+//while(i<pixelCount){
+//unsigned char average = (colors[i]+ colors[i+1] + colors[i+2])/3; //RGB
+////average = 20;
+//colors[i] = truncateBW(average);//truncate(colors[i]*0.5);
+//colors[i+1] = truncateBW(average);//truncate(colors[i+1]*0.5);
+//colors[i+2] = truncateBW(average);//truncate(colors[i+2]*0.5);
+//i+=4;
+//}
+//(*env).ReleaseIntArrayElements(pixels,pixels_,0);
+//
+//}
--- app/src/main/java/com/example/phoroeditorapplication2/MainActivity.java
+++ app/src/main/java/com/example/phoroeditorapplication2/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.ActivityManager;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
@@ -27,6 +29,7 @@ import com.example.phoroeditorapplication2.databinding.ActivityMainBinding;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
@@ -44,24 +47,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        //set layout
         setContentView(R.layout.activity_main);
         init();
     }
 
-    /**
-     * A native method that is implemented by the 'phoroeditorapplication2' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
-
+    //permissions
     private static final int REQUEST_PERMISSIONS = 1234;
     private static final String[] PERMISSIONS = {
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.WRITE_EXTERNAL_STORAGE
     };
     private static final int PERMISSION_COUNT = 2;
-
     @SuppressLint("NewApi")
     private boolean notPermissions() {
         for (int i = 0; i < PERMISSION_COUNT; i++) {
@@ -73,15 +70,18 @@ public class MainActivity extends AppCompatActivity {
         return false;
     }
 
-
+    //black and white method
     private static native void blackAndWhite(int pixels[],int width,int height);
+
     @Override
     protected void onResume() {
         super.onResume();
+        //if we don't have permissions,we ask for them
         if (notPermissions() && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ) {
             requestPermissions(PERMISSIONS, REQUEST_PERMISSIONS);
         }
     }
+
     //check granted or denied
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
@@ -91,6 +91,18 @@ public class MainActivity extends AppCompatActivity {
                 ((ActivityManager)this.getSystemService(ACTIVITY_SERVICE)).clearApplicationUserData();
                 recreate();
             }
+        }
+    }
+
+    public void onBackPressed(){
+        if(editMode){
+            findViewById(R.id.editScreen).setVisibility(View.GONE);
+            findViewById(R.id.welcomeScreen).setVisibility(View.VISIBLE);
+            editMode = false;
+        }
+        else{
+            //not in edit mode => exit the application
+            super.onBackPressed();
         }
     }
 
@@ -166,7 +178,51 @@ public class MainActivity extends AppCompatActivity {
                 }.start();
             }
         });
+        final Button saveButton = findViewById(R.id.saveImage);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                final DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        if(which == DialogInterface.BUTTON_POSITIVE) {
+                            final File outputFile = createImageFile();
+                            try (FileOutputStream fos = new FileOutputStream(outputFile)) {
+                                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                                imageUri = Uri.parse("file://" + outputFile.getAbsolutePath());
+                                sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, imageUri));
+                                Toast.makeText(MainActivity.this, "Image Saved", Toast.LENGTH_SHORT).show();
+
+                            } catch (FileNotFoundException e) {
+                                e.printStackTrace();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                };
+              builder.setMessage("Do you want to save the image?")
+                      .setPositiveButton("Yes",dialogClickListener)
+                      .setNegativeButton("No",dialogClickListener)
+                      .show();
+            }
+        });
+
+        final Button backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+               findViewById(R.id.editScreen).setVisibility(View.GONE);
+               findViewById(R.id.welcomeScreen).setVisibility(View.VISIBLE);
+               editMode = false;
+            }
+        });
     }
+
+
 
     private static final String appID = "photoEditor";
     private Uri imageUri;
